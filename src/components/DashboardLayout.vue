a601f663ecac37d75f25f4e95420c289318b932a
lawrenxiusbenny <lawrenxiusbenny@gmail.com>
2020-11-11 12:44:14 +0700

    UGD Vue CLI

--- src/components/DashboardLayout.vue
+++ src/components/DashboardLayout.vue
@@ -41,7 +41,7 @@ export default{
             items:[
                 { title: "Dashboard", to: "/" },
                 { title: "GD", to: "/gd" },
-                { title: "UGD", to: "/ugd" },
+                { title: "UGD", to: "/UGD" },
             ],
         };
     },
